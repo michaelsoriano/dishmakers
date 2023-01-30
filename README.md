35b589b084c67959bf4eaf890212159e8b1f5aff
michaelsoriano <mks6804@gmail.com>
2023-01-29 16:54:38 -0800

    removed app dir, added getserversession

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 npx prisma migrate dev --name name-of-migration
 
 testing new user commit
+
+
+
+# NEXT 13
+
+
+- Server and Client components -> https://beta.nextjs.org/docs/rendering/server-and-client-components
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./auth/[...nextauth]"
+
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const session = await getServerSession(req, res, authOptions)
+
+  console.log(session);
+
   res.status(200).json({ name: 'John Doe' })
 }
